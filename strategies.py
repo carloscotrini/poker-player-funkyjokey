ec1ad4e295929ebec769bc9e9cda09bb40792b4c
infsec-t460p <ccarlos@inf.ethz.ch>
2017-08-26 11:33:10 +0200

    .

--- strategies.py
+++ strategies.py
@@ -11,6 +11,8 @@ def isPair(our_cards, community_cards):
 def possessPair(our_cards, community_cards):
     our_ranks = [card["rank"] for card in our_cards]
     comm_ranks = [card["rank"] for card in community_cards]
-    print(str(our_ranks))
-    print(str(comm_ranks))
     return isPair(our_cards, []) or set(our_ranks) & set(comm_ranks) != set()
+
+def possessHighCard(our_cards, community_cards):
+    print({"J", "Q", "K", "A"} & set(map(lambda card : card["rank"], our_cards)))
+    return {"J", "Q", "K", "A"} & set(map(lambda card : card["rank"], our_cards)) != set()
--- strategies_test.py
+++ strategies_test.py
@@ -33,5 +33,27 @@ class StrategyTester(unittest.TestCase):
         self.assertTrue(strategies.isPair(our_cards, community_cards))
         self.assertFalse(strategies.possessPair(our_cards, community_cards))
 
+    def test_possesHighCard(self):
+
+        our_cards = [{"rank": "A"}, {"rank": "5"}]
+        community_cards = [{"rank": "A"}, {"rank": "A"}]
+        self.assertTrue(strategies.possessHighCard(our_cards, community_cards))
+
+        our_cards = [{"rank": "1"}, {"rank": "5"}]
+        community_cards = [{"rank": "A"}, {"rank": "A"}]
+        self.assertFalse(strategies.possessHighCard(our_cards, community_cards))
+
+        our_cards = [{"rank": "J"}, {"rank": "5"}]
+        community_cards = [{"rank": "A"}, {"rank": "A"}]
+        self.assertTrue(strategies.possessHighCard(our_cards, community_cards))
+
+        our_cards = [{"rank": "2"}, {"rank": "5"}]
+        community_cards = [{"rank": "1"}, {"rank": "1"}]
+        self.assertFalse(strategies.possessHighCard(our_cards, community_cards))
+
+        our_cards = [{"rank": "1"}, {"rank": "3"}]
+        community_cards = [{"rank": "A"}, {"rank": "A"}]
+        self.assertFalse(strategies.possessHighCard(our_cards, community_cards))
+
 if __name__ == "__main__":
     unittest.main()
