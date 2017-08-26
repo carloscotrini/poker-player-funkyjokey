21668a149c01bea9e81c61dfb2c4f475006a51c8
Stefano Baghino <stefano@baghino.me>
2017-08-26 10:41:17 +0200

    refactoring

--- player.py
+++ player.py
@@ -3,39 +3,7 @@ import strategies
 class Player:
     VERSION = "Default Python folding player"
 
-    """
-    game state:
-        - players
-          [
-           - name
-             stack int
-             status
-             bet int
-             hole_cards
-             [
-               - rank 2-10 or J Q K A
-               - suit [hearts diamonds clubs spades]
-             ]
-             version
-             id
-          ]
-        - tournament_id
-        - game_id
-        - round int
-        - bet_index int
-        - small_blind int
-        - orbits
-        - dealer
-        - community_cards [cards]
-        - current buy in
-        - pot
-    """
-
     def betRequest(self, game_state):
-        """
-        return integer: amount of chips to be bet
-        0 means check
-        """
         players = game_state['players']
         current_buy_in = game_state['current_buy_in']
         in_action = game_state['in_action']
@@ -43,9 +11,11 @@ class Player:
         community_cards = game_state['community_cards']
         bet = players[in_action]['bet']
         minimum_to_play = current_buy_in - bet
+        print(str(our_cards), str(community_cards))
         if (strategies.isPair(our_cards, community_cards)):
-            print("we have a pair", str(our_cards), str(community_cards))
+            print("PICKED STRATEGY: isPair")
             return minimum_to_play
+        print("NO STRATEGY, WE ARE CHECKING OR FOLDING")
         return 0
 
     def showdown(self, game_state):
