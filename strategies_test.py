6a227c16a7330f29d96ab9a08d1b1220717312ca
infsec-t460p <ccarlos@inf.ethz.ch>
2017-08-26 10:35:15 +0200

    .

--- strategies_test.py
+++ strategies_test.py
@@ -1,21 +1,28 @@
 import strategies
+import unittest
 
-our_cards = [{"rank": "3"}, {"rank":"3"}]
-community_cards = []
+class StrategyTester(unittest.TestCase):
 
-print(str(strategies.isPair(our_cards, community_cards)) + " == 1")
-print(str(strategies.possessPair(our_cards, community_cards)) + " == 1")
+    def test_isPair(self):
+        our_cards = [{"rank": "3"}, {"rank":"3"}]
+        community_cards = []
 
-our_cards = [{"rank": "3"}, {"rank":"5"}]
+        self.assertTrue(strategies.isPair(our_cards, community_cards))
+        self.assertTrue(strategies.possessPair(our_cards, community_cards))
 
-print(str(strategies.isPair(our_cards, community_cards)) + " == 0")
+        our_cards = [{"rank": "3"}, {"rank":"5"}]
 
-our_cards = [{"rank": "3"}, {"rank":"5"}]
-community_cards = [{"rank" : "4"}, {"rank": "5"}]
+        self.assertFalse(strategies.isPair(our_cards, community_cards))
 
-print(str(strategies.isPair(our_cards, community_cards)) + " == 1")
+        our_cards = [{"rank": "3"}, {"rank":"5"}]
+        community_cards = [{"rank" : "4"}, {"rank": "5"}]
 
-our_cards = [{"rank": "3"}, {"rank":"5"}]
-community_cards = [{"rank" : "4"}, {"rank": "A"}]
+        self.assertTrue(strategies.isPair(our_cards, community_cards))
 
-print(str(strategies.isPair(our_cards, community_cards)) + " == 0")
+        our_cards = [{"rank": "3"}, {"rank":"5"}]
+        community_cards = [{"rank" : "4"}, {"rank": "A"}]
+
+        self.assertFalse(strategies.isPair(our_cards, community_cards))
+
+if __name__ == "__main__":
+    unittest.main()
