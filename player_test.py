38e5a962be2a03513702b5361e97848206acdbfa
Stefano Baghino <stefano@baghino.me>
2017-08-26 11:21:36 +0200

    tests

--- player_test.py
+++ player_test.py
@@ -4,24 +4,23 @@ import json
 
 with open('test_case1.json') as test_case:
     bet = Player().betRequest(json.loads(test_case.read()))
-    if bet is not 0:
+    if bet != 0:
         sys.exit('problem in parsing')
 
 with open('test_case2.json') as test_case:
     bet = Player().betRequest(json.loads(test_case.read()))
-    if bet is not 0:
+    if bet != 0:
         sys.exit('betting on nothing')
 
 with open('test_case3.json') as test_case:
     bet = Player().betRequest(json.loads(test_case.read()))
-    if bet is not 3:
-        print(bet)
-        sys.exit('bet is not 3')
+    if bet != 503:
+        sys.exit('bet is not 503 but {}'.format(bet))
 
 with open('test_case4.json') as test_case:
     bet = Player().betRequest(json.loads(test_case.read()))
-    if bet is not 0:
-        sys.exit('bet is not 0')
+    if bet != 0:
+        sys.exit('bet is not 0 but {}'.format(bet))
 
 print('ALL TESTS ARE GOOD!')
 
